a231e54ed335f6a5769864582a2a458249477f75
alherondelle <agathe.lherondelle@mines-paristech.fr>
2020-10-21 16:17:30 +0200

    image generation issue

--- model.py
+++ model.py
@@ -142,8 +142,6 @@ for epoch in range(opt.start_epoch, opt.end_epoch):
         torch.save(ae.state_dict(), './conv_autoencoder_{}.pth'.format(epoch))
         pic = output[0].cpu().detach()
         real_pic = img_[0].cpu().detach()
-        pic = torch.from_numpy(np.moveaxis(np.array(pic),0, -1))
-        real_pic = torch.from_numpy(np.moveaxis(np.array(real_pic), 0, -1))
         save_image(pic, './image_{}.png'.format(epoch))
         save_image(real_pic, './image_real_{}.png'.format(epoch))
 
