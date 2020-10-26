8f691dff94eac9c68c99b3e54d3b9d0554a531e8
alherondelle <agathe.lherondelle@mines-paristech.fr>
2020-10-26 17:20:08 +0100

    padding error rectification

--- model_image.py
+++ model_image.py
@@ -66,13 +66,12 @@ class Autoencoder(nn.Module):
         super(Autoencoder, self).__init__()
         ## encoder layers ##
         # conv layer (depth from 1 --> 16), 3x3 kernels
-        self.conv1 = nn.Conv2d(2, 16, 3, padding=1)  
+        self.conv1 = nn.Conv2d(2, 16, 3, padding = 2)  
         # conv layer (depth from 16 --> 4), 3x3 kernels
         self.conv2 = nn.Conv2d(16, 8, 3, padding=1)
         self.conv3 = nn.Conv2d(8, 4, 3, padding=1)
         # pooling layer to reduce x-y dims by two; kernel and stride of 2
         self.pool = nn.MaxPool2d(2, 2)
-        
         ## decoder layers ##
         ## a kernel of 2 and a stride of 2 will increase the spatial dims by 2
         self.t_conv1 = nn.ConvTranspose2d(4, 8, 2, stride=2)
@@ -95,13 +94,15 @@ class Autoencoder(nn.Module):
         x = F.relu(self.conv3(x))
         x = self.pool(x)
         print('conv3: ',x.shape) # => compressed representation
-
         ## decode ##
         # add transpose conv layers, with relu activation function
         x = F.relu(self.t_conv1(x))
+        print('t_conv1: ',x.shape) 
         x = F.relu(self.t_conv2(x))
+        print('t_conv2: ',x.shape) 
         # output layer (with sigmoid for scaling from 0 to 1)
         x = torch.sigmoid(self.t_conv3(x))
+        print('t_conv3: ',x.shape) 
         return x
 
 
@@ -118,7 +119,7 @@ iter_per_epoch = len(data_loader)
 data_iter = iter(data_loader)
 
 # Training
-"""
+
 for epoch in range(opt.start_epoch, opt.end_epoch):
     t0 = time()
     for i, img in tqdm(enumerate(data_loader)):
@@ -144,7 +145,6 @@ for epoch in range(opt.start_epoch, opt.end_epoch):
 
 # Saving trained model : Final
 torch.save(ae.state_dict(), './conv_autoencoder_model_v2_{}.pth'.format(epoch))
-"""
 # Stopping train phase & Separating encoder / decoder 
 ae.eval()
 ae_encoder_keys = ["conv1.weight", "conv1.bias", "conv2.weight", "conv2.bias", "conv3.weight", "conv3.bias", 'pool.weight', 'pool.biais']
